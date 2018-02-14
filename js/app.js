dd3f264cb1a407db351fe2563dc727d517e2bd8f
cristy22mx <cristy22mx@gmail.com>
2018-02-14 11:57:43 -0600

    estructura html

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+var textoIngresado = document.getElementById("ingresoTexto").value;
+
+
+function myFunctionLarge(){
+var textoIngresado = document.getElementById("ingresoTexto").value;
+document.getElementById("result").i = textoIngresado;
+}
