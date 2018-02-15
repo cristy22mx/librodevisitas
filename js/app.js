50d4b075dab78ea85cb623bf20760571cfeeced4
cristy22mx <cristy22mx@gmail.com>
2018-02-15 11:20:43 -0600

    funcionalidades de todos los botones

--- js/app.js
+++ js/app.js
@@ -1,34 +1,86 @@
+/*Variable donde se ingresa el texto del usuario*/
+
+var textUser = document.getElementById("ingresoTexto");
+textUser.addEventListener("keyup", showPreview);
+
+/*Variable donde se refleja lo que el usuario ingreso*/
+
+var textPreview = document.getElementById("preview");
 
 
-document.getElementById("large").addEventListener("click", functionLarge);
+//Función para reflejar texto en textarea
+function showPreview() {
+    textPreview.textContent = textUser.value;
+}
+
+/*Función botón large*/
+
+var buttonLarge = document.getElementById("large").addEventListener("click", functionLarge);
 
 function functionLarge() {
-	var addText = document.getElementById("ingresoTexto").value;
-	document.getElementById("result").innerHTML = addText;
-    document.getElementById("result").style.fontSize = 18 + "pt";
+    textPreview.style.fontSize = 18 + "pt";
 }
 
-document.getElementById("medium").addEventListener("click", functionmedium);
+/*Función botón medium*/
 
-function functionmedium() {
-	var addText = document.getElementById("ingresoTexto").value;
-	document.getElementById("result").innerHTML = addText;
-    document.getElementById("result").style.fontSize = 12 + "pt";
+var buttonmedium = document.getElementById("medium").addEventListener("click", functionMedium);
+
+function functionMedium() {
+    textPreview.style.fontSize = 12 + "pt";
 }
 
-document.getElementById("small").addEventListener("click", functionsmall);
+/*Función botón large*/
 
-function functionsmall() {
-	var addText = document.getElementById("ingresoTexto").value;
-	document.getElementById("result").innerHTML = addText;
-    document.getElementById("result").style.fontSize = 8 + "pt";
+var buttonsmall = document.getElementById("small").addEventListener("click", functionSmall);
+
+function functionSmall() {
+    textPreview.style.fontSize = 8 + "pt";
 }
 
+/*Función botón color letra*/
 
+var textColor = document.getElementById("colorText").addEventListener("click", functionColor);
 
+function functionColor() {
+	var colorText = prompt("Escribe de qué color quieres tu texto");
+    textPreview.style.color = colorText;
+}
 
+/*Función botón Backgroun color*/
 
+var backgroundColor = document.getElementById("fondo").addEventListener("click", functionBackgroun);
 
+function functionBackgroun() {
+	var colorBackground = prompt("Escribe de que color quiere tu fondo");
+    textPreview.style.backgroundColor = colorBackground;
+}
+
+/*Función botón left*/
+
+var buttonleft = document.getElementById("left")
+buttonleft.addEventListener("click", functionLeft);
+
+function functionLeft() {
+    textPreview.style.textAlign = "left";
+}
+
+/*Función botón center*/
+
+var buttoncenter = document.getElementById("center")
+buttoncenter.addEventListener("click", functionCenter);
+
+function functionCenter() {
+    textPreview.style.textAlign = "center";
+}
+
+/*Función botón right*/
+
+var buttonright = document.getElementById("right")
+buttonright.addEventListener("click", functionRight);
+
+function functionRight() {
+    textPreview.style.textAlign = "right";
+}
 
 
 
