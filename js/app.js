749f1d098c1ef9d27971e848be1848b0d3fc75bf
cristy22mx <cristy22mx@gmail.com>
2018-02-14 19:12:19 -0600

    función small - medium - large

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 
-var textoIngresado = document.getElementById("ingresoTexto").value;
 
+document.getElementById("large").addEventListener("click", functionLarge);
 
-function myFunctionLarge(){
-var textoIngresado = document.getElementById("ingresoTexto").value;
-document.getElementById("result").i = textoIngresado;
+function functionLarge() {
+	var addText = document.getElementById("ingresoTexto").value;
+	document.getElementById("result").innerHTML = addText;
+    document.getElementById("result").style.fontSize = 18 + "pt";
 }
+
+document.getElementById("medium").addEventListener("click", functionmedium);
+
+function functionmedium() {
+	var addText = document.getElementById("ingresoTexto").value;
+	document.getElementById("result").innerHTML = addText;
+    document.getElementById("result").style.fontSize = 12 + "pt";
+}
+
+document.getElementById("small").addEventListener("click", functionsmall);
+
+function functionsmall() {
+	var addText = document.getElementById("ingresoTexto").value;
+	document.getElementById("result").innerHTML = addText;
+    document.getElementById("result").style.fontSize = 8 + "pt";
+}
+
